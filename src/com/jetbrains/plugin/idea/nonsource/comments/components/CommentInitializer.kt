356d5b22474582f92a1060f8de52fdb3a3687573
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-10-19 22:12:41 +0300

    ENH: comment annotation in gutter is clickable now

--- src/com/jetbrains/plugin/idea/nonsource/comments/components/CommentInitializer.kt
+++ src/com/jetbrains/plugin/idea/nonsource/comments/components/CommentInitializer.kt
@@ -3,10 +3,13 @@ package com.jetbrains.plugin.idea.nonsource.comments.components
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.EditorGutterAction
+import com.intellij.openapi.editor.LogicalPosition
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
 import com.jetbrains.plugin.idea.nonsource.comments.listeners.EditorCaretListener
 import com.jetbrains.plugin.idea.nonsource.comments.services.CommentService
+import java.awt.Cursor
 
 /**
  * @author demiurg
@@ -26,7 +29,19 @@ class CommentInitializer : ProjectComponent, Disposable {
                 if (project == null) {
                     return
                 }
-                editor.gutter.registerTextAnnotation(CommentGutterAnnotation(CommentService.getInstance(project)))
+                editor.gutter.registerTextAnnotation(CommentGutterAnnotation(CommentService.getInstance(project)),
+                        object : EditorGutterAction {
+                            val commentService = CommentService.getInstance(project)
+
+                            override fun doAction(line: Int) {
+                                editor.caretModel.currentCaret.moveToLogicalPosition(LogicalPosition(line, 0, true))
+                                commentService.toolbarEditor.grabFocus()
+                            }
+
+                            override fun getCursor(lineNum: Int): Cursor? {
+                                return editor.component.cursor
+                            }
+                        })
             }
 
             override fun editorReleased(event: EditorFactoryEvent) {
