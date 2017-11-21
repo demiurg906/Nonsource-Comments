f551acbbf532ad4a5ee043e031d00d2d0f6ca040
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-11-21 11:44:33 +0300

    MAINT: inlays looks pretty

--- src/com/jetbrains/plugin/idea/nonsource/comments/components/InlayCommentRenderer.kt
+++ src/com/jetbrains/plugin/idea/nonsource/comments/components/InlayCommentRenderer.kt
@@ -1,14 +1,18 @@
 package com.jetbrains.plugin.idea.nonsource.comments.components
 
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorCustomElementRenderer
 import com.intellij.openapi.editor.impl.ComplementaryFontsRegistry
+import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.editor.impl.FontInfo
+import com.intellij.util.ui.GraphicsUtil
 import com.intellij.xdebugger.ui.DebuggerColors
 import com.jetbrains.plugin.idea.nonsource.comments.model.Comment
 import java.awt.Font
 import java.awt.Graphics
 import java.awt.Rectangle
+import javax.swing.UIManager.getFont
 
 /**
  * @author demiurg
@@ -30,17 +34,46 @@ class InlayCommentRenderer(val comment: Comment) : EditorCustomElementRenderer {
         if (comment.text == "") {
             return
         }
-        val attributes = editor.colorsScheme.getAttributes(DebuggerColors.INLINED_VALUES_EXECUTION_LINE) ?: return
-        val fgColor = attributes.foregroundColor ?: return
-        g.color = fgColor
+
+        val attributes = editor.colorsScheme.getAttributes(DefaultLanguageHighlighterColors.INLINE_PARAMETER_HINT) ?: return
+
         val fontInfo = getFontInfo(editor)
-        g.font = fontInfo.getFont()
         val metrics = fontInfo.fontMetrics()
-        g.drawString(comment.text, r.x, r.y + metrics.getAscent())
+        val font = fontInfo.font
+        val lineHeight = Math.ceil(font.createGlyphVector(metrics.fontRenderContext, "Ap").visualBounds.height).toInt()
+        val BACKGROUND_ALPHA = 0.55f
+
+        val backgroundColor = attributes.backgroundColor
+        if (backgroundColor != null) {
+            val config = GraphicsUtil.setupAAPainting(g)
+            GraphicsUtil.paintWithAlpha(g, BACKGROUND_ALPHA)
+            g.color = backgroundColor
+            val gap = if (r.height < lineHeight + 2) 1 else 2
+            g.fillRoundRect(r.x + 2, r.y + gap, r.width - 4, r.height - gap * 2, 8, 8)
+            config.restore()
+        }
+        val foregroundColor = attributes.foregroundColor
+        if (foregroundColor != null) {
+            g.color = foregroundColor
+            g.font = getFont(editor)
+            val savedClip = g.clip
+            g.clipRect(r.x + 3, r.y + 2, r.width - 6, r.height - 4)
+            val editorAscent = (editor as? EditorImpl)?.ascent ?: 0
+            g.drawString(comment.text, r.x + 7, r.y + Math.max(editorAscent, (r.height + metrics.ascent - metrics.descent) / 2) - 1)
+            g.clip = savedClip
+        }
+
+//        val attributes = editor.colorsScheme.getAttributes(DebuggerColors.INLINED_VALUES_EXECUTION_LINE) ?: return
+//        val fgColor = attributes.foregroundColor ?: return
+//        g.color = fgColor
+//        val fontInfo = getFontInfo(editor)
+//        g.font = fontInfo.font
+//        val metrics = fontInfo.fontMetrics()
+//        g.drawString(comment.text, r.x, r.y + metrics.ascent)
     }
 
     override fun calcWidthInPixels(editor: Editor): Int {
         val fontInfo = getFontInfo(editor)
-        return maxOf(1, fontInfo.fontMetrics().stringWidth(comment.text))
+        return maxOf(1, fontInfo.fontMetrics().stringWidth(comment.text) + 14)
     }
 }
