28f29cef21f79825f210b756dd6d5748e5fb31a6
Dmitriy Novozhilov <dimon2x@gmail.com>
2017-12-30 21:12:08 +0300

    MAINT: little refactoring of actions

---
+++ src/com/jetbrains/plugin/idea/nonsource/comments/actions/AbstractIntentionAction.kt
@@ -0,0 +1,26 @@
+package com.jetbrains.plugin.idea.nonsource.comments.actions
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+/**
+ * @author demiurg
+ *         30.12.2017
+ */
+
+abstract class AbstractIntentionAction : IntentionAction {
+    override fun getFamilyName(): String {
+        return "Nonsource comments"
+    }
+
+    override fun startInWriteAction(): Boolean {
+        return false
+    }
+
+    override fun isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean {
+        // TODO: мб стоит прочекать, что ничего не грузится
+        return file.virtualFile.exists()
+    }
+}
